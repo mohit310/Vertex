7c9559d4f4b72f397ac4832402d5c504e8c3f422
Mohit Keswani <mohit.keswani@gmail.com>
2016-03-31 21:20:44 -0400

    modifed for test case to work fine

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-java -cp ./target/vertex-1.0-SNAPSHOT-jar-with-dependencies.jar com.insight.mk.hashtag.Application "$@"
+java -cp ./target/vertex-1.0-SNAPSHOT-jar-with-dependencies.jar com.insight.mk.hashtag.Application ./tweet_input/tweets.txt ./tweet_output/output.txt
 
 
